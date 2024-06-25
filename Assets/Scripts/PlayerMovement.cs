9237055e9d1b6c9db316af224ae0c24bed420533
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-06-26 00:54:49 +0100

    Movement is now smoothened

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float playerMoveSpeed = 15.0f;
+    [SerializeField] float maxSpeed = 10.0f;
+    [SerializeField] float accTime = 0.2f;
+    [SerializeField] float damageAccTime = 0.0f;
 
     private PlayerInput playerInput;
     private Rigidbody2D rigidbodyComp;
 
     private Vector2 moveVector;
 
+    private Vector2 smoothVelDir;
+    private Vector2 currentAccDir;
+
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rigidbodyComp = GetComponent<Rigidbody2D>();   
+
+        
 
         //Add the "Move" function to the callback for the "Move" Input action
         playerInput.actions["Move"].performed += Move;
@@ -26,12 +34,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        smoothVelDir = Vector2.SmoothDamp(smoothVelDir, moveVector, ref currentAccDir, accTime + damageAccTime);
 
+        rigidbodyComp.velocity = new Vector3(smoothVelDir.x, smoothVelDir.y) * maxSpeed;
     }
 
     private void Move(InputAction.CallbackContext context)
     {
         moveVector = context.ReadValue<Vector2>();
-        rigidbodyComp.velocity = moveVector * playerMoveSpeed;
+        //rigidbodyComp.velocity = moveVector * playerMoveSpeed;
     }
 }
